dec2a789927b2761ec8d977271baa88d6e1d4262
chouhan789 <59730777+chouhan789@users.noreply.github.com>
2020-01-31 16:45:02 +0530

    Create greatest among three no.s

---
+++ greatest among three no.s
@@ -0,0 +1,9 @@
+num1=10
+num2=15
+num3=16
+if(num1>num2)and(num1>num3):
+    print("num1 is largest")
+elif(num2>num3)and(num2>num1):
+    print("num2 is largest")
+else:
+    print("num3 is largest")
